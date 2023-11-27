8dad883dcd2395b5c0262161a1884ec23ebde56e
Felixx <phai.nguyen@coccoc.com>
2023-11-27 18:57:53 +0700

    Move elemet declaration into a types file

--- scripts/sync.js
+++ scripts/sync.js
@@ -1,9 +1,3 @@
-
-
-
-// ===============================================
-
-
 // declaration
 const TAB_ID = `${Date.now()}_${Math.round(Math.random()*100000)}`;
 
@@ -13,42 +7,6 @@ function message(type, data) {
         id: TAB_ID,
         type,
         data
-    }
-}
-
-class UIComponent {
-    #el;
-    #proxy;
-
-    text;
-
-    constructor(selectorOrElement) {
-        if (typeof selectorOrElement == 'string') {
-            this.#el = document.querySelector(selectorOrElement);
-        } else {
-            this.#el = selectorOrElement;
-        }
-
-        this.text = '';
-
-        const ctx = this;
-
-        this.#proxy = new Proxy(this, {
-            set(target, prop, value) {
-                if (prop == 'text') {
-                    ctx.#el.innerText = value;
-                }
-                return Reflect.set(target, prop, value);
-            }
-        });
-
-        return this.#proxy;
-    }
-}
-
-class CanvasComponent extends UIComponent {
-    constructor(selectorOrElement) {
-        super(selectorOrElement);
     }
 }
 
---
+++ scripts/types/element.js
@@ -0,0 +1,35 @@
+class UIComponent {
+    #el;
+    #proxy;
+
+    text;
+
+    constructor(selectorOrElement) {
+        if (typeof selectorOrElement == 'string') {
+            this.#el = document.querySelector(selectorOrElement);
+        } else {
+            this.#el = selectorOrElement;
+        }
+
+        this.text = '';
+
+        const ctx = this;
+
+        this.#proxy = new Proxy(this, {
+            set(target, prop, value) {
+                if (prop == 'text') {
+                    ctx.#el.innerText = value;
+                }
+                return Reflect.set(target, prop, value);
+            }
+        });
+
+        return this.#proxy;
+    }
+}
+
+class CanvasComponent extends UIComponent {
+    constructor(selectorOrElement) {
+        super(selectorOrElement);
+    }
+}
