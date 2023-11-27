425be4f8bc01e07851943ba3685bd84c4578ca95
Felixx <phai.nguyen@coccoc.com>
2023-11-27 19:03:36 +0700

    move drawer to new file

--- scripts/sync.js
+++ scripts/sync.js
@@ -29,34 +29,3 @@ window.addEventListener('beforeunload', () => {
 bc.postMessage(message('connect'));
 const tid = new UIComponent('title');
 tid.text = `Client ${TAB_ID}`;
-/**
- * @type HTMLCanvasElement
- */
-const $canvas = document.querySelector('#app');
-(function draw() {
-    const ctx = $canvas.getContext("2d");
-    const center = {
-        x: 95,
-        y: 50,
-    }
-    ctx.beginPath();
-    ctx.arc(center.x, center.y, 40, 0, 2 * Math.PI);
-    ctx.stroke();
-
-    const screen = window.screen;
-    let isLeftSide = screen.availWidth / 2.2 > window.screenLeft;
-    if (isLeftSide) {
-        const screenRight = window.screenLeft + window.innerWidth;
-        ctx.beginPath();
-        ctx.moveTo(center.x, center.y)
-        ctx.lineTo(screenRight - center.x, center.y)
-        ctx.stroke();
-    } else {
-        ctx.beginPath();
-        ctx.moveTo(center.x, center.y)
-        ctx.lineTo(0, center.y)
-        ctx.stroke();
-    }
-})();
-
-// window.addEventListener('')
