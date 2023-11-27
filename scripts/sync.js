0deb00606d2cbb327d4b2548046508b2f3562de2
Felixx <phai.nguyen@coccoc.com>
2023-11-27 19:07:17 +0700

    optimize sync file

--- scripts/sync.js
+++ scripts/sync.js
@@ -1,31 +1,28 @@
 // declaration
 const TAB_ID = `${Date.now()}_${Math.round(Math.random()*100000)}`;
-
-// utilization
-function message(type, data) {
-    return {
-        id: TAB_ID,
-        type,
-        data
-    }
-}
-
-// initialization
 const bc = new BroadcastChannel('app_sync');
 
-bc.addEventListener('message', (event) => {
-    console.log(event);
-});
-
-window.addEventListener('beforeunload', () => {
-    if (bc) {
-        bc.postMessage(message('disconnect'))
-        bc.close();
+(function() {
+    function message(type, data) {
+        return {
+            id: TAB_ID,
+            type,
+            data
+        }
     }
-});
+    bc.addEventListener('message', (event) => {
+        console.log(event);
+    });
+    
+    window.addEventListener('beforeunload', () => {
+        if (bc) {
+            bc.postMessage(message('disconnect'))
+            bc.close();
+        }
+    });
 
-
-// app
-bc.postMessage(message('connect'));
-const tid = new UIComponent('title');
-tid.text = `Client ${TAB_ID}`;
+    // app
+    bc.postMessage(message('connect'));
+    const tid = new UIComponent('title');
+    tid.text = `Client ${TAB_ID}`;
+})();
