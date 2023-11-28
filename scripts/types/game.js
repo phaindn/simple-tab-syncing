9ad632f7fb1883688fbd000ce9aac59b2c2e82c6
Felixx <phai.nguyen@coccoc.com>
2023-11-28 18:48:05 +0700

    update game component

--- scripts/types/game.js
+++ scripts/types/game.js
@@ -1,3 +1,9 @@
+/**
+ * @typedef {Object} Coordinate
+ * @property {number} x
+ * @property {number} y
+ */
+
 class BaseObject {
     /**
      * @type number
@@ -44,23 +50,30 @@ class BaseObject {
     }
 }
 
-class Shooter extends BaseObject {
-    
-}
-
 class Tower extends BaseObject {
     /**
-     * @type Shooter
+     * @type {Shooter}
      */
     gun;
+
+    /**
+     * @constant
+     * @type {Coordinate}
+     */
+    ATTACH_POSITION;
+
     constructor() {
         super(120, 120, '/assets/Tower.png');
+        this.ATTACH_POSITION = {
+            x: 60,
+            y: 60,
+        }
     }
 
     /**
      * @param {number} degree 
      */
-    rotate(degree) {
+    rotateGun(degree) {
     }
 
     /**
@@ -73,5 +86,131 @@ class Tower extends BaseObject {
         }
         this.gun = gun;
     }
+
 }
 
+class Shooter extends BaseObject {
+    /**
+     * @type {number}
+     */
+    speed;
+
+    /**
+     * @type {number}
+     */
+    angle;
+
+    /**
+     * @type {number}
+     */
+    level;
+
+    /**
+     * @constant
+     * @type {Coordinate}
+     */
+    ATTACH_POSITION;
+
+    /**
+     * @type {Bullet}
+     */
+    Bullet;
+
+    /**
+     * @param {number} width 
+     * @param {number} height 
+     * @param {string} image 
+     * @param {number} speed 
+     * @param {Bullet} bullet 
+     */
+    constructor(width, height, image, speed, bullet) {
+        super(width, height, image);
+        this.speed = speed;
+        this.Bullet = bullet;
+        this.level = 1;
+    }
+
+    /**
+     * @overload
+     * @param {number} width 
+     * @param {number} height 
+     * @param {string} image 
+     * @param {number} speed 
+     * @param {Bullet} bullet 
+     */
+    constructor(width, height, image, speed, angle, bullet) {
+        super(width, height, image);
+        this.speed = speed;
+        this.angle = angle;
+        this.Bullet = bullet;
+        this.level = 1;
+    }
+
+    /**
+     * @overload
+     * @param {number} width 
+     * @param {number} height 
+     * @param {string} image 
+     * @param {number} speed 
+     * @param {Bullet} bullet 
+     * @param {number} attachPositionX 
+     * @param {number} attachPositionY 
+     */
+    constructor(width, height, image, speed, angle, bullet, attachPositionX, attachPositionY) {
+        super(width, height, image);
+        this.speed = speed;
+        this.angle = angle;
+        this.Bullet = bullet;
+        this.level = 1;
+        this.ATTACH_POSITION = {
+            x: attachPositionX,
+            y: attachPositionY,
+        }
+    }
+
+    /**
+     * @param {Tower} tower 
+     */
+    attachTo(tower) {
+        tower.attach(tower);
+    }
+
+    shoot() {
+        let b = this.#createBullet()
+    }
+
+    #createBullet() {
+    }
+}
+
+class Bullet extends BaseObject {
+    /**
+     * @type {number}
+     */
+    speed;
+    /**
+     * @type {number}
+     */
+    damage;
+    /**
+     * @type {number}
+     */
+    angle;
+    /**
+     * @type {Coordinate}
+     */
+    position;
+
+    /**
+     * @param {number} width 
+     * @param {number} height 
+     * @param {string} image 
+     * @param {number} speed 
+     * @param {number} damage 
+     */
+    constructor(width, height, image, speed, damage) {
+        super(width, height, image);
+        this.speed = speed;
+        this.damage = damage
+    }
+}
